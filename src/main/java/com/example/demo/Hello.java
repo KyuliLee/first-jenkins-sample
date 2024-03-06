b5efd90b0311751ead881c3cfcb2e85f475c3136
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2024-03-06 10:31:52 +0900

    will turn on jenkins

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+
 public class Hello {
     String today = "march 6th while turning on jenkins";
+    public String getToday(){
+        return today;
+    }
+
 }
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     
-    @GetMapping("/")
+//    @GetMapping(value = "/", produces = {MediaType.APPLICATION_JSON_UTF_VALUE})
+//    @GetMapping(value = "/", produces = {MediaType.APPLICATION_JSON})
+    @GetMapping(value = "/")
     public Hello home() {
         return new Hello();
     }
